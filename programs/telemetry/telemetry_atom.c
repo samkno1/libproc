95d5e4df168c2a160acc6c7d1286b67b09ac2ed4
samkno1 <sm619375@dal.ca>
2023-03-17 03:14:56 -0300

    process for telemetry collection

--- programs/telemetry/telemetry_atom.c
+++ programs/telemetry/telemetry_atom.c
@@ -8,8 +8,17 @@
 
 #define TEMP_PATH "/sys/bus/i2c/devices/4-004c/temp1_input"
 #define TELEMETRY_PATH "/root/telemetry"
+
 #define POST_INTERVAL 21700000
 #define RECORD_INTERVAL 60000
+
+enum TelemetryType {
+  kTemp = 0,
+  kAdcs = 1,
+};
+
+// used only for getting adcs telemetry
+static struct TelemetryInfo *telem;
 
 void get_obc_temp(float *temp) {
   int temp_fd;
@@ -32,58 +41,95 @@ void get_obc_temp(float *temp) {
   return;
 }
 
-int record_telemetry(struct ProcessData *proc, void *arg) {
-  int flags, bytes;
-  char buff[1024];
+void receive_adcs_telemetry(int socket, unsigned char cmd, void *data,
+                            size_t dataLen, struct sockaddr_in *fromAddr) {
+  int res = 0;
 
+  struct AdcsTelemetry *adcs;
+  adcs = (struct AdcsTelemetry *)data;
+
+  if (_write_telemetry(telem->proc, telem, kAdcs) < 0) {
+    DBG_print(DBG_LEVEL_WARN, "Unable to write telemetry\n");
+  }
+}
+
+// internal function for writing telemetry to file
+int _write_telemetry(ProcessData *proc, struct TelemetryInfo *telemetry,
+                     enum TelemetryType typ) {
+  // write temperature
+  int res, buff_size;
+  char buff[256];
+
+  if (typ == kTemp) {
+    buff_size =
+        snprintf(buff, 50, "OBC Temperature: %f\n", telemetry->obc_temp);
+  } else if (typ = kAdcs) {
+    buff_size = snprintf(buff, 50, "ADCS active devices #: %d\n",
+                         telemetry->adcs_info.num_active);
+  }
+
+  res = PROC_nonblocking_write(proc, telemetry->telemetry_fd, buff, buff_size,
+                               FREE_DATA_AFTER_WRITE);
+
+  return res;
+}
+
+int record_telemetry(ProcessData *proc, void *arg) {
   struct TelemetryInfo *telemetry = (struct TelemetryInfo *)arg;
 
-  // temp
   get_obc_temp(telemetry->obc_temp);
 
-  // create telemetry file
-  flags = O_WRONLY | O_CREAT | O_APPEND | O_NONBLOCK;
-  telemetry->telemetry_fd = open(TELEMETRY_PATH, flags);
-
-  // write temperature
-  snprintf(buff, 50, "OBC Temperature: %f\n", telemetry->obc_temp);
-  if (!(bytes = write(telemetry->telemetry_fd, buff, sizeof(buff)))) {
-    DBG_print(DBG_LEVEL_INFO, "Wrote %d bytes\n", bytes);
+  if (!PROC_cmd(proc, "telemetry?", *proc, sizeof(*proc), "adcs")) {
+    DBG_print(DBG_LEVEL_WARN, "Unable to send command to adcs\n");
   }
 
-  if (!close(telemetry->telemetry_fd)) {
-    DBG_print(DBG_LEVEL_WARN, "Unable to close file\n");
+  if (_write_telemetry(proc, telemetry, kTemp) < 0) {
+    DBG_print(DBG_LEVEL_WARN, "Unable to write telemetry\n");
   }
+  return EVENT_KEEP;
+}
 
+int sigint_handler(int signum, void *arg) {
+  DBG("SIGINT handler!\n");
+  EVT_exit_loop(PROC_evt(arg));
   return EVENT_KEEP;
 }
 
 int main(void) {
-  // Where libproc stores its state
-  struct ProcessData *proc;
-
   // telemetry parameters
   struct TelemetryInfo telemetry;
 
+  // initialize structure
+  // memset(telemetry, 0, sizeof(telemetry));
+  telemetry.obc_temp = -1;
+
   // Initialize the process
-  proc = PROC_init("telemetry", WD_ENABLED);
+  telem = &telemetry;
+  telemetry.proc = PROC_init("telemetry", WD_ENABLED);
   if (!proc) {
     DBG_print(DBG_LEVEL_FATAL, "FAILED TO INITIALIZE PROCESS\n");
     return -1;
   }
-  DBG_init(proc->name);
+
+  DBG_init();
+
+  // initialize file descriptor
+  telemetry.telemetry_fd =
+      open(TELEMETRY_PATH, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND,
+           S_IRUSR | S_IWUSR);
 
   // Setup a scheduled and signal event
-  EVT_sched_add(PROC_evt(proc), EVT_ms2tv(RECORD_INTERVAL), &record_telemetry,
-                &telemetry);
+  EVT_sched_add(PROC_evt(telemetry.proc), EVT_ms2tv(RECORD_INTERVAL),
+                &record_telemetry, &telemetry);
+  PROC_signal(telemetry.proc, SIGINT, &sigint_handler, telemetry.proc);
 
   // Start the event loop
   DBG_print(DBG_LEVEL_INFO, "Collecting telemetry...\n");
-  EVT_start_loop(PROC_evt(proc));
+  EVT_start_loop(PROC_evt(telemetry.proc));
 
   // Cleanup liproc
   DBG_print(DBG_LEVEL_INFO, "Cleaning up process...\n");
-  PROC_cleanup(proc);
+  PROC_cleanup(telemetry.proc);
 
   return 0;
 }
--- programs/telemetry/telemetry_atom.h
+++ programs/telemetry/telemetry_atom.h
@@ -1,4 +1,31 @@
 #include "polysat.h"
 
-int telemetry_event(void);
-int post_telemetry(void);
+#ifndef TELEMETRY_H_
+#define TELEMETRY_H_
+
+struct TelemetryInfo {
+  float obc_temp;
+  struct adcs_telemetry adcs_info;
+  int telemetry_fd;
+  ProcessData *proc;
+};
+
+// Telemetry for Adcs subsystem. Still under construction.
+struct AdcsTelemetry {
+  int num_active;
+};
+
+// get OBC's one and only telemetry: temperature
+void get_obc_temp(float *temp);
+
+// record telemetry for OBC and request telemetry from Adcs
+int record_telemetry(ProcessData *proc, void *arg);
+
+// callback for Adcs process
+// records Adcs telemetry received
+void receive_adcs_telemetry(int socket, unsigned char cmd, void *data,
+                            size_t dataLen, struct sockaddr_in *fromAddr);
+
+// used in debugging
+int sigint_handler(int signum, void *arg);
+#endif
